f69e5d653ee9a686d7695b8944c268cd02b366f8
Justin <justin27sackprasit@gmail.com>
2022-05-01 22:11:45 +1000

    Add functionality to operator buttons
    The 'equals' button isn't functional yet, but the correct calculations
    are still made when the operator buttons are pressed. The code
    calculates the first pair of numbers before the next number is inputted.

--- script.js
+++ script.js
@@ -17,28 +17,25 @@ function divide(a, b) {
 function operate(operator, num1, num2) {
   switch (operator) {
     case '+':
-      add(num1, num2);
-      break;
+      return add(num1, num2);
     case '-':
-      subtract(num1, num2);
-      break;
+      return subtract(num1, num2);
     case '*':
-      multiply(num1, num2);
-      break;
+      return multiply(num1, num2);
     case '/':
-      divide(num1, num2);
-      break;
-    default:
-      alert("Not a valid input.");
+      return divide(num1, num2);
   }
 }
 
 let calc_display = document.querySelector('.calc-display');
+let current_calculation = 0;
+// Follows the format = {num1: 0, operator: '', num2: 0}
+let current_expression = {};
+// Holds the the current number before the user clicks an operator
+let current_number = '';
 
 //  Adds click event listeners to each numbered button which adds them to the calculator display
-
 let nums = document.querySelectorAll('.num');
-
 nums.forEach(num => {
   if (num.classList.contains('b-one')) {
     addNumberEvent(num, 1);
@@ -64,10 +61,48 @@ nums.forEach(num => {
 });
 
 function addNumberEvent(button, digit) {
-  button.addEventListener('click', () => calc_display.textContent += digit);
+  button.addEventListener('click', () => {
+    if (current_number === '') {
+      calc_display.textContent = '';
+    }
+    calc_display.textContent += digit;
+    current_number += digit;
+  });
 }
 
-// let b_one = document.querySelector('.b-one');
-// b_one.addEventListener('click', () => {
-//   calc_display.textContent += '1';
-// });
+let ops = document.querySelectorAll('.operator');
+ops.forEach(op => {
+  if (op.classList.contains('o-plus')) {
+    addOperatorEvent(op, '+');
+  } else if (op.classList.contains('o-minus')) {
+    addOperatorEvent(op, '-');
+  } else if (op.classList.contains('o-multiply')) {
+    addOperatorEvent(op, '*');
+  } else if (op.classList.contains('o-divide')) {
+    addOperatorEvent(op, '/');
+  }
+});
+
+function addOperatorEvent(button, operator) {
+  button.addEventListener('click', () => {
+    // Checks if anything is in current_expression and sets .num1 and .operator
+    // (this condition should only be true the first time an operator
+    // is clicked in a calculation)
+    if (!('num1' in current_expression)) {
+      current_expression.num1 = Number(current_number);
+      current_expression.operator = operator;
+      current_number = '';
+    } else {
+      // Sets .num2 in current_expression and calls operate() with the values
+      // in current_expression, then sets .num1 with the new calcalution and
+      // .operator with the new operator
+      current_expression.num2 = Number(current_number);
+      current_calculation = operate(current_expression.operator, current_expression.num1, current_expression.num2);
+      current_expression.num1 = current_calculation;
+      current_expression.operator = operator;
+      // Deletes .num2 in current_expression and resets the current_number
+      delete current_expression.num2;
+      current_number = '';
+    }
+  });
+}

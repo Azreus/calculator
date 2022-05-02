94d5920e1f0d5beb728043ff70e85262651c1bc6
Justin <justin27sackprasit@gmail.com>
2022-05-02 20:31:25 +1000

    Add functionality to the 'equals' button
    Other certain conditions were also covered for (e.g. if the user were to
    press '=' if there was only 1 operand and an operator in
    current_expression, it would use the first number for the 2nd operand to
    make the calculation).

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function operate(operator, num1, num2) {
 }
 
 let calc_display = document.querySelector('.calc-display');
+
 let current_calculation = 0;
 // Follows the format = {num1: 0, operator: '', num2: 0}
 let current_expression = {};
@@ -103,6 +104,33 @@ function addOperatorEvent(button, operator) {
       // Deletes .num2 in current_expression and resets the current_number
       delete current_expression.num2;
       current_number = '';
+      calc_display.textContent = current_calculation;
     }
   });
 }
+
+const equals = document.querySelector('.b-equals');
+equals.addEventListener('click', () => {
+  // If there's currently a number but no operator pressed
+  if (Object.keys(current_expression).length === 0 && !(current_number === '')) {
+    return;
+  } else if (('num1' in current_expression && 'operator' in current_expression) &&
+      current_number === '') {
+    // If there's a number and operator but no current number: (num1 operator num1 = )
+    current_expression.num2 = current_expression.num1;
+    current_calculation = operate(current_expression.operator, current_expression.num1, current_expression.num2);
+    calc_display.textContent = current_calculation;
+    current_expression = {};
+    current_number = '';
+  } else if (('num1' in current_expression && 'operator' in current_expression) &&
+      !(current_number === '')) {
+    // If there's 2 numbers and operator: (num1 operator num2 = )
+    current_expression.num2 = Number(current_number);
+    current_calculation = operate(current_expression.operator, current_expression.num1, current_expression.num2);
+    calc_display.textContent = current_calculation;
+    current_expression = {};
+    current_number = '';
+  } else {
+    calc_display.textContent = current_calculation;
+  }
+});
